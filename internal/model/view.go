82e82a920101daea7bbd5f6ad04699630cdc23b7
hirotake111 <hirotake111@users.noreply.github.com>
2025-10-29 15:33:46 +0900

    refactor

--- internal/model/view.go
+++ internal/model/view.go
@@ -2,14 +2,15 @@ package model
 
 import (
 	"github.com/charmbracelet/lipgloss"
+	"github.com/hirotake111/redisclient/internal/component"
 	"github.com/hirotake111/redisclient/internal/component/helpbox"
-	"github.com/hirotake111/redisclient/internal/keylist"
 )
 
 const (
 	appShellPadding   = 2
 	helpBoxHeight     = 3
 	helpBoxPaddingTop = 1
+	heightErrorBox    = 5 // Space for error messages
 )
 
 var (
@@ -20,21 +21,36 @@ var (
 func (m Model) View() string {
 	width := m.width - appShellPadding*2
 	height := m.height - appShellPadding*2 - helpBoxHeight - helpBoxPaddingTop
-	helpBox := helpbox.New(helpBoxHeight)
 
-	app := keylist.Render(
-		m.mode,
-		width,
-		height,
-		m.mode.Keys,
-		m.HostName(),
-		m.State,
-	)
+	heightValueDisplay := height - heightErrorBox - 7
+	heightLeftPane := heightValueDisplay + heightErrorBox + 2
+	widthLeftPane := width / 3
+	widthRightPane := width - widthLeftPane - 5
 
-	return appShellStyle.Render(
-		lipgloss.JoinVertical(lipgloss.Top,
-			app,
-			helpBoxStyle.Render(helpBox),
-		),
-	)
+	// Help box
+	helpBox := helpBoxStyle.Render(helpbox.New(helpBoxHeight))
+
+	// Database tab
+	tab := component.TabRow(m.mode.Tabs, m.mode.CurrentTab)
+
+	// Message box
+	msgbox := component.ErrorBox(m.mode.ErrorMsg, widthRightPane, heightErrorBox)
+
+	// Key list
+	left := m.mode.KeyList.View(widthLeftPane, heightLeftPane, m.State)
+
+	// Viewport
+	viewport := m.mode.Viewport.View(widthRightPane, heightValueDisplay, m.State)
+
+	// Connection display
+	bottom := component.HostHeader(m.HostName())
+
+	// Right pane
+	right := lipgloss.JoinVertical(lipgloss.Top, viewport, msgbox)
+
+	middle := lipgloss.JoinHorizontal(lipgloss.Top, left, right)
+
+	main := lipgloss.JoinVertical(lipgloss.Left, tab, middle, bottom, helpBox)
+
+	return appShellStyle.Render(main)
 }
